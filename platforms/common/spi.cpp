0a212d2bd140dd10a30cd27188c49e91551ed093
wangluhe <574486440@qq.com>
2025-08-29 10:56:16 +0800

    修改spi.cpp

--- platforms/common/spi.cpp
+++ platforms/common/spi.cpp
@@ -87,6 +87,8 @@ const px4_spi_bus_t *px4_spi_buses{nullptr};
 
 int px4_find_spi_bus(uint32_t devid)
 {
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Waddress"
 	for (int i = 0; ((px4_spi_bus_t *) px4_spi_buses) != nullptr && i < SPI_BUS_MAX_BUS_ITEMS; ++i) {
 		const px4_spi_bus_t &bus_data = px4_spi_buses[i];
 
